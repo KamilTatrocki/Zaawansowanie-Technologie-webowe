4a2d1dcfb05ee2f6cf58ecfc98dc2a3249f25a97
hashibane <hook060620@gmail.com>
2026-04-14 16:21:08 +0200

    Coomit

--- lista5/library/src/main/java/org/example/library/config/GlobalExceptionHandler.java
+++ lista5/library/src/main/java/org/example/library/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.library.config;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +8,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleDataViolationException(BadRequestException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataViolationException(DataIntegrityViolationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body("Could not complete the operation. Check if entity referenced by in other entities.");
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
