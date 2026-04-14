398a2722b5d6f1e8a5f9101e7ede2b99e88009d9
KamilTatrocki <kamil.tatrocki@outlook.com>
2026-04-14 17:19:45 +0200

    camel

--- lista5/library/src/main/java/org/example/library/controller/BookCopyController.java
+++ lista5/library/src/main/java/org/example/library/controller/BookCopyController.java
@@ -18,7 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/book-copies")
+@RequestMapping("/api/bookCopies")
 @RequiredArgsConstructor
 public class BookCopyController {
 
