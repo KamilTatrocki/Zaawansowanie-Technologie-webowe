39d21d3395e15f8d99a2a865ba21763ad48b392b
KamilTatrocki <kamil.tatrocki@outlook.com>
2026-04-19 20:59:47 +0200

    delete another layout

--- lista5/library_frontend/src/router/index.ts
+++ lista5/library_frontend/src/router/index.ts
@@ -14,61 +14,61 @@ const router = createRouter({
       path: '/books',
       name: 'books',
       component: () => import('../views/BooksView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/books/:id',
       name: 'book-detail',
       component: () => import('../views/BookDetailView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/authors',
       name: 'authors',
       component: () => import('../views/AuthorsView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/authors/:id',
       name: 'author-detail',
       component: () => import('../views/AuthorDetailView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/readers',
       name: 'readers',
       component: () => import('../views/ReadersView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/readers/:id',
       name: 'reader-detail',
       component: () => import('../views/ReaderDetailView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/book-copies',
       name: 'book-copies',
       component: () => import('../views/BookCopiesView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/book-copies/:id',
       name: 'book-copy-detail',
       component: () => import('../views/BookCopyDetailView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/rentals',
       name: 'rentals',
       component: () => import('../views/RentalsView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/rentals/:id',
       name: 'rental-detail',
       component: () => import('../views/RentalDetailView.vue'),
-      meta: { layout: 'AppLayoutTable' }
+      meta: { layout: 'AppLayoutHome' }
     },
     {
       path: '/test',
